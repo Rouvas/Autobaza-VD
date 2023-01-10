9fcac9d227ca4ca1bb4c1ea605167b57b6e03f6f
Rouvas <mail@gaiduchik.com>
2023-01-10 03:29:28 +0300

    Initial commit

---
+++ ClientApp/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCNwnOO-vqxIkIGFAi0UEQeD7KBVIy_jw8",
+    authDomain: "autobaza-vd.firebaseapp.com",
+    databaseURL: 'https://autobaza-vd-default-rtdb.europe-west1.firebasedatabase.app/',
+    projectId: "autobaza-vd",
+    storageBucket: "autobaza-vd.appspot.com",
+    messagingSenderId: "1040010105572",
+    appId: "1:1040010105572:web:98add088c2bd9ec4d652f1",
+  }
+};
---
+++ ClientApp/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCNwnOO-vqxIkIGFAi0UEQeD7KBVIy_jw8",
+    authDomain: "autobaza-vd.firebaseapp.com",
+    databaseURL: 'https://autobaza-vd-default-rtdb.europe-west1.firebasedatabase.app/',
+    projectId: "autobaza-vd",
+    storageBucket: "autobaza-vd.appspot.com",
+    messagingSenderId: "1040010105572",
+    appId: "1:1040010105572:web:98add088c2bd9ec4d652f1",
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
